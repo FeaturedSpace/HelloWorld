64bfc89664bcf1ad6c14af9218f07ecf4040b89b
camdenspehl <camdenspehl@camdens-mbp>
2019-04-28 14:28:34 -0400

    Added basic Tools2Plugin project.

---
+++ TestProject/src/com/featuredspace/plugins/Tools2/Tools2Plugin.java
@@ -0,0 +1,17 @@
+package com.featuredspace.plugins.Tools2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Tools2Plugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		this.getLogger().info("We're loaded.");
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
