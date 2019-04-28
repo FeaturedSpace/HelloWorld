0f21b196ac6a259863c061d5590728567ea42ce9
camdenspehl <camdenspehl@camdens-mbp>
2019-04-28 14:50:51 -0400

    Commands stufs.

--- TestProject/src/com/featuredspace/plugins/Tools2/Tools2Plugin.java
+++ TestProject/src/com/featuredspace/plugins/Tools2/Tools2Plugin.java
@@ -1,5 +1,9 @@
 package com.featuredspace.plugins.Tools2;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Tools2Plugin extends JavaPlugin {
@@ -12,6 +16,23 @@ public class Tools2Plugin extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if(command.getName().equalsIgnoreCase("fly")) {
+			
+			if(sender instanceof Player) {
+				Player player = (Player) sender;
+				
+				player.setAllowFlight(!player.getAllowFlight());
+				player.sendMessage(ChatColor.YELLOW + "Toggled FlyMode");
+			}
+			
+		}
+		
+		return true;
 	}
 	
 }
